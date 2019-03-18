3e23851f769f96c44302d194640905824288975f
weixun.lwx <weixun.lwx@ruishanio.com>
2019-03-18 17:51:35 +0800

    init

---
+++ app/src/main/java/picasso/utils/junx/com/picassoutils/PicassoUtils.java
@@ -0,0 +1,17 @@
+package picasso.utils.junx.com.picassoutils;
+
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class PicassoUtils {
+    public static void load(String url,ImageView iv){
+        if (iv==null)
+            throw new NullPointerException("imageview is null!!");
+
+        Picasso picasso = new Picasso.Builder(iv.getContext())
+                .build();
+        picasso.load(url)
+                .into(iv);
+    }
+}
