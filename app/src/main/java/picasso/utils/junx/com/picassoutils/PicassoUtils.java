a85161e80204d2ac6ff544dc750e10c548a059b6
weixun.lwx <weixun.lwx@ruishanio.com>
2019-03-22 11:38:14 +0800

    retry

--- app/src/main/java/picasso/utils/junx/com/picassoutils/PicassoUtils.java
+++ app/src/main/java/picasso/utils/junx/com/picassoutils/PicassoUtils.java
@@ -6,7 +6,6 @@ import com.squareup.picasso.Picasso;
 
 public class PicassoUtils {
     public static void load(String url,ImageView iv){
-
         if (iv==null)
             throw new NullPointerException("imageview is null!!");
 
